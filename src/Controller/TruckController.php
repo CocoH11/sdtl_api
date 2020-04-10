90c91273ef74bb289f51a6c5c900c0bcbaa32d50
corentin <corentin.holcvart@hotmail.fr>
2020-04-10 14:07:22 +0200

    Fixture Files generated for Type and Homeagency entities

--- src/Controller/TruckController.php
+++ src/Controller/TruckController.php
@@ -2,27 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Truck;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TruckController extends AbstractController
 {
-    /**
+    /*/**
      * @Route("/truck", name="truck")
      */
-    public function index()
+    /*public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/TruckController.php',
         ]);
     }
-
+*/
     /**
-     * @Route("/truck", name="addTruck", methods={"PUT"})
+     * @Route("/truck", name="addTruck", methods={"POST"})
      */
     public function addTruck(Request $request){
+        $newtruck= new Truck();
+
+        $newtruck->setNumberplate();
+        $newtruck->addCode();
+        $newtruck->setHomeagency();
+        $newtruck->setType();
+        return new Response("addTruck", 200);
 
     }
 
@@ -30,14 +39,14 @@ class TruckController extends AbstractController
      * @Route("truck", name="deleteTruck", methods={"DELETE"})
      */
     public function deleteTruck(Request $request){
-
+        return new Response("deleteTruck", 200);
     }
 
     /**
      * @Route("truck", name="updateTruck", methods={"PATCH"})
      */
     public function updateTruck(Request $request){
-
+        return new Response("updateTruck", 200);
     }
 
 
---
+++ src/DataFixtures/HomeagencyFixture.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Homeagency;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class HomeagencyFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $dataHomeagencies=[
+            ["name"=>"TC58"],
+            ["name"=>"TC35"],
+            ["name"=>"TC59"],
+            ["name"=>"TC14"],
+            ["name"=>"TC71"],
+            ["name"=>"TC63"],
+            ["name"=>"TC60"],
+            ["name"=>"TC91"],
+            ["name"=>"SDTL"],
+            ["name"=>"ALG"],
+
+        ];
+
+        foreach ($dataHomeagencies as $homeagency){
+            $newHomeagency=new Homeagency();
+            $newHomeagency->setName($homeagency["name"]);
+        }
+
+        $manager->flush();
+    }
+}
