7c38cc8aa4c600af87d78f848f7cfd866bb3bcd0
corentin <corentin.holcvart@hotmail.fr>
2020-04-14 13:54:51 +0200

    Configuration of each entity to use the validator component

--- src/Controller/DriverController.php
+++ src/Controller/DriverController.php
@@ -7,13 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class DriverController extends AbstractController
 {
     /**
      * @Route("/driver", name="addDriver", methods={"PUT"})
      */
-    public function addDriver(Request $request){
+    public function addDriver(Request $request, ValidatorInterface $validator){
         $data=json_decode($request->getContent(), true);
         //Doctrine
         $doctrine=$this->getDoctrine();
@@ -24,6 +25,18 @@ class DriverController extends AbstractController
             ->setName($data["driver"]["name"])
             ->setFirstname($data["driver"]["firstname"])
         ;
+        $errors = $validator->validate($newdriver);
+
+        if (count($errors) > 0) {
+            /*
+             * Uses a __toString method on the $errors variable which is a
+             * ConstraintViolationList object. This gives us a nice string
+             * for debugging.
+             */
+            //$errorsString = (string) $errors;
+
+            return new Response("c'est pas bien");
+        }
         $doctrine->getManager()->persist($newdriver);
         $doctrine->getManager()->flush();
         return new Response("", 200);
