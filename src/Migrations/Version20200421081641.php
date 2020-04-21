f941dd8d3bc258e52c9b1dea00d6354885b9dc0e
corentin <corentin.holcvart@hotmail.fr>
2020-04-21 10:23:39 +0200

    Truck numberplate has been modified, now it's a string and not an integer

---
+++ src/Migrations/Version20200421081641.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200421081641 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE integrationmodel CHANGE codedriverlocation codedriverlocation VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE refuel CHANGE driver_id driver_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE truck CHANGE numberplate numberplate VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE homeagency_id homeagency_id INT DEFAULT NULL, CHANGE roles roles JSON NOT NULL, CHANGE api_token api_token VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE integrationmodel CHANGE codedriverlocation codedriverlocation VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE refuel CHANGE driver_id driver_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE truck CHANGE numberplate numberplate INT NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE homeagency_id homeagency_id INT DEFAULT NULL, CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`, CHANGE api_token api_token VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
+    }
+}
