0f5dde3d3dc7a061fecd657b8db8e07924f4eda4
corentin <corentin.holcvart@hotmail.fr>
2020-04-22 11:12:51 +0200

    We can take the list of available systems through the getSystems method, this method return only the name and the id of each system object

---
+++ src/Controller/SystemController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\System;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class SystemController
+ * @package App\Controller
+ * @Route("/api")
+ */
+class SystemController extends AbstractController
+{
+    /**
+     * @Route("/systems", name="getSystems", methods={"GET"})
+     */
+    public function getSystems(Request $request){
+        $systems=$this->getDoctrine()->getRepository(System::class)->findAll();
+        $datatosend=[];
+
+        foreach ($systems as $system){
+            array_push($datatosend, ["id"=>$system->getId(), "name"=>$system->getName()]);
+        }
+        return new JsonResponse($datatosend, 200);
+    }
+}
